eee668354976c51b85f1ce167cac8bf950a67162
glorieya <110552604+glorieya@users.noreply.github.com>
2022-10-17 14:23:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,46 @@
 from typing import List
 
-def quick_sort(data, low, high) -> List[int]:
-    # Write code here
+def merge(arr: [int], l: int, mid: int, r: int) -> [int]:
+  #Function implementing merging of two sorted sub-arrays
+  #Input: arr -> array
+  #       l: -> start index
+  #       mid: -> middle index
+  #       r: -> right index
+  l1 = arr[l:mid]
+  l2 = arr[mid:r]
+  i, j = 0, 0
+  k =  l
+  while i < len(l1) and j < len(l2):
+    if l1[i] < l2[j]:
+      arr[k] = l1[i]
+      i += 1
+    else:
+      arr[k] = l2[j]
+      j += 1
+    k += 1
+  while i < len(l1):
+    arr[k] = l1[i]
+    i += 1
+    k += 1
+  while j < len(l2):
+    arr[k] - l2[j]
+    j += 1
+    k += 1
+
+def merge_sort_ftn(arr, l, r) -> None:
+  #Function implementing in-place merge sort of subarray arr[l:r]
+  if r - l >= 2:
+    mid = l + (r - l) // 2
+    merge_sort_ftn(arr, l, mid)
+    merge_sort_ftn(arr, mid, r)
+    merge(arr, l, mid, r)
+  
+def merge_sort(data) -> None:
+  #TFunction carrying out in-place sorting of given array
+  merge_sort_ftn(data, 0, len(data))
 
 
+# Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -11,4 +48,5 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(quick_sort(data, 0, len(data)-1))
+merge_sort(data)
+print(data)
